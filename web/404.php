c52f9da6580061ffd62c9637185dd7300e5adfb5
siddharth-techie-vyas <sid.vyas786@gmail.com>
2025-02-13 18:15:38 +0530

    404

---
+++ web/404.php
@@ -0,0 +1,37 @@
+<!-- .page-title -->
+        <div class="page-title relative">
+            <div class="paralaximg" data-parallax="scroll" data-image-src="images/page-title/page-title-7.jpg">
+            </div>
+            <div class="content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <h3 class="title">Faqs</h3>
+                            <ul class="breadcrumb">
+                                <li><a href="index_3997808.html">Homepage</a></li>
+                                <li>Pages</li>
+                                <li>Faq</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!-- /.page-title -->
+
+        <!-- .faqs -->
+        <section class="flat-spacing">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="page-faqs-wrap">
+                            <div class="list-faqs">
+                                
+                                <img src="<?php echo $base_url.'assets/images/404.jpg'?>"/>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </section><!-- .faqs -->

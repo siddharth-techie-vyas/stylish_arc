6d91f9f00fc9fce0f7a294fc14c36a444823d98c
WebituteTechnologies <Web@#230>
2025-04-06 15:11:25 +0530

    no image found

--- web/shop.php
+++ web/shop.php
@@ -132,14 +132,14 @@
                         <div class="card-product style-1 grid" data-availability="Out of stock" data-brand="adidas">
                             <div class="card-product-wrapper">
                                 <a href="<?php echo $base_url.'index.php?action=dashboard&page=product-detail&product_id='.$v['id'].'&product-name='.$v['product_name'];?>" class="image-wrap">
-                                    <?php if(file_exists($base_url.'assets/images/'.$v['picture'])){?>
+                                    <?php if(!file_exists($base_url.'assets/images/'.$v['picture'])){?>
                                     <img class="lazyload img-product" data-src="<?php echo $base_url.'assets/images/'.$v['picture']; ?>"
                                         src="<?php echo $base_url.'assets/images/'.$v['picture']; ?>" alt="image-product">
                                     <img class="lazyload img-hover" data-src="<?php echo $base_url.'assets/images/'.$v['picture']; ?>"
                                         src="<?php echo $base_url.'assets/images/'.$v['picture']; ?>" alt="image-product">
                                     <?php }else{?>
                                         <img class="lazyload img-product" data-src="<?php echo $base_url.'assets/images/no-image.png'; ?>"
-                                        src="<?php echo $base_url.'assets/images/no-image.png'; ?>" alt="image-product">
+                                        src="<?php echo $base_url.'assets/images/noimage_found.jpg'; ?>" alt="image-product">
                                     <?php }?>         
                                 </a>
                                 <div class="list-product-btn">
